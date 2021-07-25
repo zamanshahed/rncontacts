3f65429d00a95c86cdcfcb09796a7764a3fe79be
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-07-25 20:03:44 +0600

    basic project organising done

--- App.js
+++ App.js
@@ -70,20 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="Step One">Binary in my Blood!</Section>
         </View>
       </ScrollView>
     </SafeAreaView>

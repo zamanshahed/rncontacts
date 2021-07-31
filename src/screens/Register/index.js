876713993f031a05f4c0a54654e3ef566b47a19f
zamanshahed <shaheduz.zaman@northsouth.edu>
2021-07-31 14:15:46 +0600

    axios-instance added

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,16 +1,23 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import envs from '../../config/env';
 
 import RegisterComponent from '../../components/Register/index';
+import axiosInstance from '../../context/helpers/axiosInterceptor';
 
 const Register = () => {
   const [form, setForm] = useState({});
   const [errors, setErrors] = useState({});
 
   // DEV environment variable urls check up
-  const {DEV_BACKEND_URL} = envs;
-  console.log('DEV_BACKEND_URL >>> ', DEV_BACKEND_URL);
-  console.log('__DEV__: ', __DEV__);
+  // const {DEV_BACKEND_URL} = envs;
+  // console.log('DEV_BACKEND_URL >>> ', DEV_BACKEND_URL);
+  // console.log('__DEV__: ', __DEV__);
+
+  React.useEffect(() => {
+    axiosInstance.get('/contacts').catch(err => {
+      console.log('error: ', err.response);
+    });
+  }, []);
 
   const onChange = ({name, value}) => {
     setForm({...form, [name]: value});
